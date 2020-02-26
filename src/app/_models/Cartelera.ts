a69abbc612cd2a5c875c3e3a999b0b3f1ddec62c
stoledo.produccion <laibach2019>
2020-02-25 23:34:49 -0300

    changes

--- src/app/_models/Cartelera.ts
+++ src/app/_models/Cartelera.ts
@@ -1,7 +1,11 @@
+import { User } from './User';
+
 export class Cartelera {
     id: number;
     titulo: string;
     descripcion: string;
     tipoCartelera: string;
     asignatura?: string;
+    alta: Date;
+    autor?: User;
 }
